2186f4fc2872ac8dde4a263480e15eb3b78ae8d6
Ethan Rowe <ROWEEH2@student.op.ac.nz>
2021-02-05 10:07:55 +1300

    Added videos of the game

---
+++ Scripts/OutOfBounds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    private CharacterController2D player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            player = collision.gameObject.GetComponent<CharacterController2D>();
+            player.die();  
+        }
+    }
+}
---
+++ Scripts/SpawnPoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    private CharacterController2D player;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            player = collision.gameObject.GetComponent<CharacterController2D>();
+            player.SpawnPoint = transform.position;
+        }
+    }
+}
